6d87c11258ca4ca64c7c3b9e23472285c466c44f
Sebastian <sebastian.asturizaga@ucb.edu.bo>
2022-08-17 23:31:50 -0400

    commit de la prueba de multiplicacion

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,15 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+
+const firstMul = document.querySelector("#primer-numeroMul");
+const secondMul = document.querySelector("#segundo-numeroMul");
+const formMul =document.querySelector("#multiplicar-form");
+const divMul= document.querySelector("#resultado-divMul");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +18,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+formMul.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumberMul = Number.parseInt(firstMul.value);
+  const secondNumberMul = Number.parseInt(secondMul.value);
+
+  divMul.innerHTML = "<p>" + multiplicar(firstNumberMul, secondNumberMul) + "</p>";
 });
